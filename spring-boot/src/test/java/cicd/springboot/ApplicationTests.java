ea98877a189c7e312b6045578c9e4ee928eb2a28
DDUSANE <63367839+DDUSANE@users.noreply.github.com>
2021-01-12 17:07:36 +0530

    Update ApplicationTests.java

--- spring-boot/src/test/java/cicd/springboot/ApplicationTests.java
+++ spring-boot/src/test/java/cicd/springboot/ApplicationTests.java
@@ -1,13 +1,30 @@
 package cicd.springboot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import cicd.springboot.controller.CICDController;
 
 @SpringBootTest
 class ApplicationTests {
+	
+	
 
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+    CICDController service;
+
+    @DisplayName("Test Spring @Autowired Integration")
+    @Test
+    void testGet() {
+        assertEquals("Hi ! This is just test message.", service.showMessage());
+    }
 
 }
